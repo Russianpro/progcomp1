5bfb55a7f1fed5afed99d12f2e55c2a20abbe1be
Russianpro <leonardogranero@outlook.com>
2025-03-26 22:35:02 -0300

    trote

--- Little kobe/scripts.js
+++ Little kobe/scripts.js
@@ -1,4 +1,4 @@
 // função que será chamada no html
-function mensagem()
+function mensagem(){
     alert("Obrigado! Agora você está me seguindo")
 }
--- Trote/js/scripts.js
+++ Trote/js/scripts.js
@@ -1,11 +1,26 @@
 function calcular(){
+    let soma = 0;
     //recupera o valor da ação social digitado
     //declarção de variãvel sem tipo
-    let acaoSocial = document.getElementById("acaoSocial").value 
-  
-    //recupera o valor da homenagem digitado
-    let homenagem = document.getElementById("homenagem").value
-    //vamos somar os valores
-    let soma = Number(acaoSocial) + Number(homenagem)
-         alert(soma)
+    let acaoSocial = Number(document.getElementById("acaoSocial").value);
+    soma += (1 * acaoSocial);
+    let homenagem = Number(document.getElementById("homenagem").value);
+    soma += (1 * homenagem);
+    let leite = Number(document.getElementById("leite").value);
+    soma += (2 * leite);
+    let kitAvulso = Number(document.getElementById("kitAvulso").value);
+    soma += (30 * kitAvulso);
+    let kitSuplementoAvulso = Number(document.getElementById("kitSuplementoAvulso").value);
+    soma += (15 * kitSuplementoAvulso);
+    let pacoteArroz = Number(document.getElementById("pacoteArroz").value);
+    soma += (1 * pacoteArroz);
+    let pacoteFeijao = Number(document.getElementById("pacoteFeijao").value);
+    soma += (1 * pacoteFeijao);
+    let pacoteMacarrao = Number(document.getElementById("pacoteMacarrao").value);
+    soma += (0.5 * pacoteMacarrao);
+    let oleo = Number(document.getElementById("oleo").value);
+    soma = soma + (1 * oleo);    
+    //devolve o resultado para HTML
+
+    document.getElementById("soma").innerHTML = soma.toFixed(2);
 }
