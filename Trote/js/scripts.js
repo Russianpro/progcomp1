da802d18765c232c339f4eeb0a5ee73b7cc5f36e
Russianpro <leonardogranero@outlook.com>
2025-04-01 19:30:06 -0300

    trote

--- Trote/js/scripts.js
+++ Trote/js/scripts.js
@@ -4,23 +4,60 @@ function calcular(){
     //declarção de variãvel sem tipo
     let acaoSocial = Number(document.getElementById("acaoSocial").value);
     soma += (1 * acaoSocial);
+
     let homenagem = Number(document.getElementById("homenagem").value);
     soma += (1 * homenagem);
+
     let leite = Number(document.getElementById("leite").value);
     soma += (2 * leite);
-    let kitAvulso = Number(document.getElementById("kitAvulso").value);
-    soma += (30 * kitAvulso);
-    let kitSuplementoAvulso = Number(document.getElementById("kitSuplementoAvulso").value);
-    soma += (15 * kitSuplementoAvulso);
+
+    let kitAlimentacao = Number(document.getElementById("kitAlimentacao").value);
+    soma += (30 * kitAlimentacao);
+
+    let equipe = document.getElementById("equipe").value
+
+        if(equipe == "Laranja"){
+            let pontosKitAlimentacao = 0
+            let pontosKitSuplmento
+        }
+        if(kitAlimentacao >= 97 && kitSuplemento >= 49){
+                pontosKitAlimentacao = 5000 + ((kitAlimentacao - 97) * 30) + ((kitSuplemento - 49) * 15);
+        }
+       
+            else if(kitAlimentacao >= 78){
+            pontosKitAlimentacao = 4000 + ((kitAlimentacao - 78) * 30);
+        }
+            else if(kitAlimentacao >= 49){
+            pontosKitAlimentacao = 2500 + ((kitAlimentacao - 49) * 30);
+        }
+            else if(kitAlimentacao >= 19){
+            pontosKitAlimentacao = 1000 + ((kitAlimentacao - 19) * 30);
+        }
+            else{
+            pontosKitAlimentacao = 0;
+        }
+             alert(pontosKitAlimentacao);
+            
+                
+
+    let kitSuplemento = Number(document.getElementById("kitSuplemento").value);
+    soma += (15 * kitSuplemento);
+
     let pacoteArroz = Number(document.getElementById("pacoteArroz").value);
     soma += (1 * pacoteArroz);
+
     let pacoteFeijao = Number(document.getElementById("pacoteFeijao").value);
     soma += (1 * pacoteFeijao);
+
     let pacoteMacarrao = Number(document.getElementById("pacoteMacarrao").value);
     soma += (0.5 * pacoteMacarrao);
+
     let oleo = Number(document.getElementById("oleo").value);
     soma = soma + (1 * oleo);    
-    //devolve o resultado para HTML
-
+    
     document.getElementById("soma").innerHTML = soma.toFixed(2);
+
+    
+
+
 }
