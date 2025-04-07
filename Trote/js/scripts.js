a4c1b68bcfbb37bb4f0fb7160f3d12719c4c4d5e
Russianpro <leonardogranero@outlook.com>
2025-04-07 20:38:37 -0300

    trote

--- Trote/js/scripts.js
+++ Trote/js/scripts.js
@@ -16,32 +16,119 @@ function calcular(){
 
     let equipe = document.getElementById("equipe").value
 
+    let kitSuplemento = Number(document.getElementById("kitSuplemento").value);
+    soma += (15 * kitSuplemento);
+
+    let pontosKitAlimentacao = 0
+   
+
         if(equipe == "Laranja"){
-            let pontosKitAlimentacao = 0
-            let pontosKitSuplmento
-        }
+            
+       
         if(kitAlimentacao >= 97 && kitSuplemento >= 49){
                 pontosKitAlimentacao = 5000 + ((kitAlimentacao - 97) * 30) + ((kitSuplemento - 49) * 15);
         }
        
-            else if(kitAlimentacao >= 78){
-            pontosKitAlimentacao = 4000 + ((kitAlimentacao - 78) * 30);
+            else if(kitAlimentacao >= 78 && kitSuplemento>= 39){
+            pontosKitAlimentacao = 4000 + ((kitAlimentacao - 78) * 30 + (kitSuplemento - 39) * 15);
         }
-            else if(kitAlimentacao >= 49){
-            pontosKitAlimentacao = 2500 + ((kitAlimentacao - 49) * 30);
+            else if(kitAlimentacao >= 49 && kitSuplemento>= 25){
+            pontosKitAlimentacao = 2500 + ((kitAlimentacao - 49) * 30 + (kitSuplemento - 25) * 15);
         }
-            else if(kitAlimentacao >= 19){
-            pontosKitAlimentacao = 1000 + ((kitAlimentacao - 19) * 30);
+            else if(kitAlimentacao >= 19 && kitSuplemento>= 10){
+            pontosKitAlimentacao = 1000 + ((kitAlimentacao - 19) * 30 + (kitSuplemento - 10) * 15);
         }
             else{
             pontosKitAlimentacao = 0;
         }
-             alert(pontosKitAlimentacao);
-            
-                
+        }
+        else if(equipe == "Preto"){
+          
+        
+        if(kitAlimentacao >= 103 && kitSuplemento >= 53){
+                pontosKitAlimentacao = 5000 + ((kitAlimentacao - 103) * 30) + ((kitSuplemento - 53) * 15);
+        }
+       
+            else if(kitAlimentacao >= 82 && kitSuplemento>= 42){
+            pontosKitAlimentacao = 4000 + ((kitAlimentacao - 82) * 30 + (kitSuplemento - 42) * 15);
+        }
+            else if(kitAlimentacao >= 52 && kitSuplemento>= 26){
+            pontosKitAlimentacao = 2500 + ((kitAlimentacao - 52) * 30 + (kitSuplemento - 26) * 15);
+        }
+            else if(kitAlimentacao >= 21 && kitSuplemento>= 10){
+            pontosKitAlimentacao = 1000 + ((kitAlimentacao - 21) * 30 + (kitSuplemento - 10) * 15);
+        }
+            else{
+            pontosKitAlimentacao = 0;
+        }
+        }        
+        else if(equipe == "Roxa"){
+        
+           
+        
+        if(kitAlimentacao >= 102 && kitSuplemento >= 51){
+                pontosKitAlimentacao = 5000 + ((kitAlimentacao - 102) * 30) + ((kitSuplemento - 51) * 15);
+        }
+       
+            else if(kitAlimentacao >= 82 && kitSuplemento>= 41){
+            pontosKitAlimentacao = 4000 + ((kitAlimentacao - 82) * 30 + (kitSuplemento - 42) * 15);
+        }
+            else if(kitAlimentacao >= 52 && kitSuplemento>= 26){
+            pontosKitAlimentacao = 2500 + ((kitAlimentacao - 52) * 30 + (kitSuplemento - 26) * 15);
+        }
+            else if(kitAlimentacao >= 19 && kitSuplemento>= 10){
+            pontosKitAlimentacao = 1000 + ((kitAlimentacao - 21) * 30 + (kitSuplemento - 10) * 15);
+        }
+            else{
+            pontosKitAlimentacao = 0;
+        }
+        }
 
-    let kitSuplemento = Number(document.getElementById("kitSuplemento").value);
-    soma += (15 * kitSuplemento);
+        else if(equipe == "Verde"){
+        
+           
+        
+            if(kitAlimentacao >= 88 && kitSuplemento >= 44){
+                    pontosKitAlimentacao = 5000 + ((kitAlimentacao - 88) * 30) + ((kitSuplemento - 44) * 15);
+            }
+           
+                else if(kitAlimentacao >= 70 && kitSuplemento>= 35){
+                pontosKitAlimentacao = 4000 + ((kitAlimentacao - 70) * 30 + (kitSuplemento - 35) * 15);
+            }
+                else if(kitAlimentacao >= 44 && kitSuplemento>= 22){
+                pontosKitAlimentacao = 2500 + ((kitAlimentacao - 44) * 30 + (kitSuplemento - 22) * 15);
+            }
+                else if(kitAlimentacao >= 18 && kitSuplemento>= 9){
+                pontosKitAlimentacao = 1000 + ((kitAlimentacao - 18) * 30 + (kitSuplemento - 9) * 15);
+            }
+                else{
+                pontosKitAlimentacao = 0;
+            }
+            }
+
+            else if(equipe == "Vermelha")
+            {
+        
+           
+        
+                if(kitAlimentacao >= 93 && kitSuplemento >= 47){
+                        pontosKitAlimentacao = 5000 + ((kitAlimentacao - 93) * 30) + ((kitSuplemento - 47) * 15);
+                }
+               
+                    else if(kitAlimentacao >= 74 && kitSuplemento>= 38){
+                    pontosKitAlimentacao = 4000 + ((kitAlimentacao - 74) * 30 + (kitSuplemento - 38) * 15);
+                }
+                    else if(kitAlimentacao >= 47 && kitSuplemento>= 24){
+                    pontosKitAlimentacao = 2500 + ((kitAlimentacao - 47) * 30 + (kitSuplemento - 24) * 15);
+                }
+                    else if(kitAlimentacao >= 19 && kitSuplemento>= 9){
+                    pontosKitAlimentacao = 1000 + ((kitAlimentacao - 19) * 30 + (kitSuplemento - 9) * 15);
+                }
+                    else{
+                    pontosKitAlimentacao = 0;
+                }
+                }
+
 
     let pacoteArroz = Number(document.getElementById("pacoteArroz").value);
     soma += (1 * pacoteArroz);
@@ -56,8 +143,5 @@ function calcular(){
     soma = soma + (1 * oleo);    
     
     document.getElementById("soma").innerHTML = soma.toFixed(2);
-
-    
-
 
 }
